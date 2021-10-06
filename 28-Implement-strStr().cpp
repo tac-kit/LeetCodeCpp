5dcf5ad49ccc4c0f0170aa15a553bd1c2288ade6
tac-kit <32474071+tac-kit@users.noreply.github.com>
2021-10-06 17:14:44 +0800

    add LC28 Solution 1.1

--- 28-Implement-strStr().cpp
+++ 28-Implement-strStr().cpp
@@ -1,5 +1,10 @@
 /*
 Solution 1: KMP, https://www.youtube.com/watch?v=GTJr8OvyEVQ
+Solution 1.1: Solution 1存在bug，当needle为"abaababaab"时建的table不对
+（不影响strStr结果正确性，因为table只起到加速作用，但会影响速度，并且在LC459中影响结果正确性）。
+这个实现参考了https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+以及https://leetcode.com/problems/implement-strstr/discuss/12956/C%2B%2B-Brute-Force-and-KMP，
+但比它们都简单。TODO: 逐行解释为什么能work。
 */
 
 #include<iostream>
@@ -53,9 +58,37 @@ public:
     }
 };
 
+class Solution11 {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.size()==0) return 0;
+        int m=haystack.size(), n=needle.size();
+        vector<int> table(n, 0);
+        int i=1, j=0;
+        while (i < n) {
+            if (needle[i] == needle[j]) table[i++] = ++j;
+            else j == 0 ? i++ : j = table[j-1];
+        }
+        i=0; j=0;
+        while (i < m) {
+            if (haystack[i] == needle[j]) {
+                i++; j++;
+                if (j == n) return i-j;
+            }else{
+                j == 0 ? i++ : j = table[j-1];
+            }
+        }
+        return -1;
+    }
+};
+
 int main() {
     Solution s;
     cout << s.strStr("hello", "ll") << endl;
     cout << s.strStr("aaaaa", "bba") << endl;
     cout << s.strStr("", "") << endl;
+    cout << s.strStr("abaababaab", "abaababaab") << endl;
+
+    Solution11 s11;
+    cout << s11.strStr("abaababaab", "abaababaab") << endl;
 }
