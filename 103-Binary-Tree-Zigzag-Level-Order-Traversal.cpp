ca3f27678671d07f51b17a9e486af62508042722
tac-kit <32474071+tac-kit@users.noreply.github.com>
2021-10-13 09:55:26 +0800

    add LC103 Solution 3

--- 103-Binary-Tree-Zigzag-Level-Order-Traversal.cpp
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -1,13 +1,15 @@
 /*
 Solution 1: 层序遍历+隔层倒序
 Solution 2: 层序遍历，输出时反转（按正常层序遍历顺序访问结点，在输出到res时判断当前层奇偶性），避免reverse。
-Solution 3: (TODO)上面两种方法的问题是都是按正常层序遍历顺序访问树的结点。应该在遍历时就按Zigzag顺序访问结点。
+Solution 3: 上面两种方法的问题是都是按正常层序遍历顺序访问树的结点，应该在遍历时就按Zigzag顺序访问结点，使用双栈实现。
+https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/33904/JAVA-Double-Stack-Solution
 */
 
 #include<iostream>
 #include<vector>
 #include<deque>
 #include<algorithm>
+#include<stack>
 
 using namespace std;
 
@@ -88,8 +90,49 @@ public:
     }
 };
 
+class Solution3 {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        stack<TreeNode*> l2rStack, r2lStack;
+        vector<vector<int>> resVec;
+        if (root == nullptr) return resVec;
+        l2rStack.push(root);
+        while (!l2rStack.empty()) {
+            vector<int> res;
+            while (!l2rStack.empty()) {
+                TreeNode *node = l2rStack.top();
+                l2rStack.pop();
+                res.push_back(node->val);
+                if (node->left) r2lStack.push(node->left);
+                if (node->right) r2lStack.push(node->right);
+            }
+            resVec.push_back(res);
+            
+            res.clear();
+            while (!r2lStack.empty()) {
+                TreeNode *node = r2lStack.top();
+                r2lStack.pop();
+                res.push_back(node->val);
+                if (node->right) l2rStack.push(node->right);
+                if (node->left) l2rStack.push(node->left);
+            }
+            if (!res.empty()) resVec.push_back(res);
+        }
+        return resVec;
+    }
+};
+
+void printVec(const vector<int>& vec) {
+    for_each(vec.cbegin(), vec.cend(), [](int num){cout << num << " ";});
+    cout << endl;
+}
+
 int main() {
     Solution s;
     TreeNode *tree = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
     s.zigzagLevelOrder(tree);
+
+    Solution3 s3;
+    auto res = s3.zigzagLevelOrder(tree);
+    for (auto line: res) printVec(line);
 }
